cfddb123b54065c32b51cd2760372111406495ef
Kishor Rana <30983917+kshrana@users.noreply.github.com>
2022-11-04 10:32:11 +0100

    refactor: Improve code clarity

--- signalboy_android/src/main/java/de/kishorrana/signalboy_android/util/ContextHelper.kt
+++ signalboy_android/src/main/java/de/kishorrana/signalboy_android/util/ContextHelper.kt
@@ -3,19 +3,16 @@ package de.kishorrana.signalboy_android.util
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Context
-import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 
 internal class ContextHelper {
     companion object {
-        fun getBluetoothAdapter(context: Context): BluetoothAdapter {
-            with(ContextCompat.getSystemService(context, BluetoothManager::class.java)) {
-                requireNotNull(this) {
-                    "Unable to obtain a BluetoothAdapter." +
-                            " Tip: BLE can be required per the <uses-feature> tag" +
-                            " in the AndroidManifest.xml"
-                }
-                return adapter
+        fun getBluetoothAdapter(context: Context): BluetoothAdapter =
+            requireNotNull(context.getSystemService<BluetoothManager>()) {
+                "Unable to obtain a BluetoothAdapter." +
+                        " Tip: BLE can be required per the <uses-feature> tag" +
+                        " in the AndroidManifest.xml"
             }
-        }
+                .adapter
     }
 }
